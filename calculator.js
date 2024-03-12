beac7505197ab4e3120dce4bee809b7d6db3f976
Sahil Patel <sahilpatel1906@gmail.com>
2024-03-12 12:39:52 -0500

    added function for odd number

--- calculator.js
+++ calculator.js
@@ -25,7 +25,12 @@ function even(a){
     return false;
 }
 
-const odd = null;
+const odd = function(a){
+    if(a % 2 != 0){
+        return true;
+    }
+    return false;
+};
 
 module.exports = { 
     sum, 
